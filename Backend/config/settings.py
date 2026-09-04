28236edf6a08706ef1a55d8fab8c70025c0bb80e
kiplah <kiplahvictor27@gmail.com>
2026-09-04 13:09:45 +0300

    Fix WhiteNoise manifest 500 error in production

--- Backend/config/settings.py
+++ Backend/config/settings.py
@@ -91,7 +91,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 MAILERS = {
     'default': {
